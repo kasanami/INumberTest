a4ed844995cfc334c34129ddf576e83538a92532
笠浪貴大 <dev.kacha@gmail.com>
2022-04-21 01:52:16 +0900

    プロジェクト作成 サンプルコード作成

---
+++ INumberTest/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double[] values = new double[5] { 1, 2, 3, 4, 5 };
+var sum = Sum(values);
+Console.WriteLine(sum);
+
+static T Sum<T>(IEnumerable<T> values) where T : INumber<T>
+{
+    T sum = T.Zero;
+    foreach (var value in values)
+    {
+        sum += value;
+    }
+    return sum;
+}
