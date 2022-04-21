4ae8492cdff248959577b92ee0814921506ea7a0
笠浪貴大 <dev.kacha@gmail.com>
2022-04-21 11:53:01 +0900

    BigDecimal 追加

---
+++ INumberTest/BigDecimal.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INumberTest
+{
+    internal struct BigDecimal : INumber<BigDecimal>
+    {
+        public static BigDecimal One => throw new NotImplementedException();
+
+        public static BigDecimal Zero => throw new NotImplementedException();
+
+        public static BigDecimal AdditiveIdentity => throw new NotImplementedException();
+
+        public static BigDecimal MultiplicativeIdentity => throw new NotImplementedException();
+
+        public static BigDecimal Abs(BigDecimal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Clamp(BigDecimal value, BigDecimal min, BigDecimal max)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Create<TOther>(TOther value) where TOther : INumber<TOther>
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal CreateSaturating<TOther>(TOther value) where TOther : INumber<TOther>
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal CreateTruncating<TOther>(TOther value) where TOther : INumber<TOther>
+        {
+            throw new NotImplementedException();
+        }
+
+        public static (BigDecimal Quotient, BigDecimal Remainder) DivRem(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Max(BigDecimal x, BigDecimal y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Min(BigDecimal x, BigDecimal y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Parse(string s, NumberStyles style, IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Parse(ReadOnlySpan<char> s, NumberStyles style, IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Parse(ReadOnlySpan<char> s, IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Parse(string s, IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal Sign(BigDecimal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryCreate<TOther>(TOther value, out BigDecimal result) where TOther : INumber<TOther>
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryParse([NotNullWhen(true)] string? s, NumberStyles style, IFormatProvider? provider, out BigDecimal result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryParse(ReadOnlySpan<char> s, NumberStyles style, IFormatProvider? provider, out BigDecimal result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, out BigDecimal result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, out BigDecimal result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(BigDecimal? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(BigDecimal? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator +(BigDecimal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator +(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator -(BigDecimal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator -(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator ++(BigDecimal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator --(BigDecimal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator *(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator /(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigDecimal operator %(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator !=(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator <(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator >(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator <=(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator >=(BigDecimal left, BigDecimal right)
+        {
+            throw new NotImplementedException();
+        }
+
+#pragma warning disable CS8765 // パラメーターの型の NULL 値の許容が、オーバーライドされたメンバーと一致しません。おそらく、NULL 値の許容の属性が原因です。
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj is BigDecimal)
+            {
+                return Equals((BigDecimal)obj);
+            }
+            return false;
+        }
+#pragma warning restore CS8765 // パラメーターの型の NULL 値の許容が、オーバーライドされたメンバーと一致しません。おそらく、NULL 値の許容の属性が原因です。
+
+        public int CompareTo(BigDecimal other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(BigDecimal other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- INumberTest/Program.cs
+++ INumberTest/Program.cs
@@ -1,7 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-double[] values = new double[5] { 1, 2, 3, 4, 5 };
+﻿
+var values = new double[] { 1, 2, 3, 4, 5 };
 var sum = Sum(values);
 Console.WriteLine(sum);
 
